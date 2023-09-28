8dd840da7cba6ae5348b5ca6d5efe8d69213d35c
Grigory Bykov <bykov.g@compmechlab.ru>
2023-09-28 06:33:12 +0300

    2 commit

---
+++ Task_4_Classwork/Matrix.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+a = np.array([[1,2,3],[4,5,6]], float)
+
+print(a[0,1])
+print(a[1,1])
+
+print(a.shape)
+print(a.dtype)
+
+b = np.array(range(10), float)
+b_reshape = b.reshape((2,5))
+
+print(b)
+print(b_reshape)
+
+print(b.tolist())
+
+def task(*args):
+    my_list = sorted(list(filter(lambda x: type(x) == int, list(args))))
+    return np.array(my_list, int)
+
+
+print(task('1',2,3.3333,1,10,'pwd','ls - la'))
